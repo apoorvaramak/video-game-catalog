dfceb85646ca6cf088422ccd63fd77f3a32a4d60
Apoorva <apoorvar@comcast.net>
2021-10-06 12:45:50 -0500

    cors again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:4000'
-    origins 'https://fathomless-ridge-61332.herokuapp.com'
+    # origins 'https://fathomless-ridge-61332.herokuapp.com'
 
     resource '*',
       headers: :any,
