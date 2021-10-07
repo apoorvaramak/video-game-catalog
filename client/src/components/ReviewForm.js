d7bdaca2d23920721fdafa0b90b6eee7ce0cd93f
Apoorva <apoorvar@comcast.net>
2021-10-07 09:26:28 -0500

    reviewform

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -47,7 +47,7 @@ function ReviewForm({reviews, setReviews, user}){
                     <option value = "2"> 2 </option>
                     <option value = "3"> 3 </option>
                     <option value = "4"> 4 </option>
-                    <option value = "5"> 5 </option>
+                    <option value = "5"> 5 </option> 
 			</select>
             <Button type = "submit">Submit</Button>
         </Form>
